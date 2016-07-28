88a522e0e0ef3c3d0d3a878e8df0404c75842ed5
LZF <279052642@qq.com>
2016-07-28 09:21:12 +0800

    first commit

---
+++ OCDemo/OCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCDemo
+//
+//  Created by lx-sh on 16/7/28.
+//  Copyright © 2016年 HuaweiSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCDemo/OCDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OCDemo
+//
+//  Created by lx-sh on 16/7/28.
+//  Copyright © 2016年 HuaweiSoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
