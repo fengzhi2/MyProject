6b4b544239d96540c04a16da10ba3affd78862a6
LZF <279052642@qq.com>
2016-07-28 09:35:24 +0800

    master

--- OCDemo/OCDemo/ViewController.m
+++ OCDemo/OCDemo/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"这是master分支");
 }
 
 - (void)didReceiveMemoryWarning {
