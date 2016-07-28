b2eb01547b27489989a6214f15c78d13d839f868
LZF <279052642@qq.com>
2016-07-28 15:15:26 +0800

    first commit

---
+++ Hahaha/Hahaha/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hahaha
+//
+//  Created by lx-sh on 16/7/28.
+//  Copyright © 2016年 HuaweiSoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"this is a test");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
